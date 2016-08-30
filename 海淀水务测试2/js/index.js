42cd8d8fe0578aeaa051af2475400516fe7d51b1
bill-mark <286449127@qq.com>
2016-08-30 18:02:15 +0800

    08.30-upload

---
+++ 海淀水务测试2/js/index.js
@@ -0,0 +1,57 @@
+window.onload=function(){
+    function setheight() {
+        var sidebar=document.getElementById('navUl');
+        var sidebar2=document.getElementById('iframe');
+        var sidebar3=document.getElementById('left');
+        sidebar2.style.height=document.documentElement.clientHeight+141+'px';           
+        sidebar.style.height=document.documentElement.clientHeight+'px';      
+    }
+    setheight();   
+    } 
+    //菜单js
+function showmenu(id) {
+    var list = document.getElementById("list"+id);
+    var menu = document.getElementById("menu"+id);
+
+    // if (list.style.display=="none") {
+    //     document.getElementById("list"+id).style.display="block";
+    //     menu.className="out-li2"
+    // }
+    // else {
+    //      document.getElementById("list"+id).style.display="none";
+    //      menu.className="out-li1"
+    // }
+    switch(list.style.display){  //switch相对快些
+        case 'none':
+             document.getElementById("list"+id).style.display="block";
+             menu.className="out-li2";
+             break;
+        case 'block':
+             document.getElementById("list"+id).style.display="none";
+             menu.className="out-li1"; 
+             break;     
+    }
+ }  
+
+
+
+//执法队伍管理专用菜单
+function showmenu2(id) {
+    var list2 = document.getElementById("list"+id);
+    var menu2 = document.getElementById("menu"+id)
+    if (list2.style.display=="none") {
+        document.getElementById("list"+id).style.display="block";
+        menu2.className="out-li4"
+    }else {
+        document.getElementById("list"+id).style.display="none";
+        menu2.className="out-li3"
+    }
+}
+//点击切换右侧页面
+
+function changeweb(id){
+         var iframe = document.getElementById('iframe');
+         var chg = id+'.html';  //拼接html文件    
+         iframe.setAttribute("src",chg); //设置对象的属性
+
+}
---
+++ 海淀水务测试2/js/pAnPai.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+	var gb = document.getElementsByClassName('down-name');
+	for(var i=0;i<gb.length;i++){  //用数组
+         gb[i].onclick=function(){
+         	this.parentNode.removeChild(this);
+         }
+	};
+     
+    var gb2 = document.getElementById('tc-guanbi'); 
+	var t1 = document.getElementById('tanchuang-wrap');
+	var t2 = document.getElementById('ss');
+	var gb3 = document.getElementById('tc-guanbi2');
+     
+ 		 gb2.onclick = function(){
+ 		 	t1.style.display='none';
+ 		    t2.style.display='none';
+ 		 };         
+        gb3.onclick = function(){
+ 		 	t1.style.display='none';
+ 		    t2.style.display='none';
+ 		 }; 
+ };
+
+function tianjia(){
+	var t1 = document.getElementById('tanchuang-wrap');
+	var t2 = document.getElementById('ss');
+	t1.style.display='block';
+	t2.style.display='block';
+}
+
+ 
+
+
+
+ 
